dfb59eb311fdcd86be29f8f5736f8d7213897ab9
Nathan Ngo <nathanj238@gmail.com>
2020-05-05 00:27:11 +1000

    Finished process_data function

--- general_utils.py
+++ general_utils.py
@@ -1,4 +1,5 @@
 from sys import argv
+from Data import Data
 
 
 ##
@@ -11,3 +12,26 @@ def process_args():
     nn_k_value = argv[3][:-2]
 
     return training_file, testing_file, algorithm, nn_k_value
+
+
+##
+# Process a data file by extracting the data from each row.
+# Output: A list of "Data" objects representing each row of the file.
+#
+def process_data(data_file, mapping=None):
+    data_arr = None
+
+    # Open up the file and create data objects.
+    # First to second last items are attributes. Last item is class value.
+    with open(data_file, "r") as f:
+        for line in f:
+            row_vals = line.strip().split(",")
+            attributes = row_vals[:-1]
+            class_value = True if row_vals[-1] == "yes" else False
+
+            if mapping:
+                data_arr.append(Data(attributes, class_value, mapping))
+            else:
+                data_arr.append(Data(attributes, class_value))
+
+    return data_arr
