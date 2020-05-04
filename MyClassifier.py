8232249ca15f5efcee7285b1b61dd5c0071e3da2
Nathan Ngo <nathanj238@gmail.com>
2020-05-05 01:23:02 +1000

    Added data processing and refactored variables

--- MyClassifier.py
+++ MyClassifier.py
@@ -2,16 +2,19 @@
 
 ##
 # Run Naive Bayes.
+# Input: 2 arrays of "Data" objects.
+# Output: Array of "True" or "False" values.
 #
-def run_nb(training, testing):
+def run_nb(training_set, testing_set):
     return
 
 
 ##
 # Run K Nearest Neighbours.
+# Input: 2 arrays of "Data" objects. An integer value K.
 # Output: Array of "True" or "False" values.
 #
-def run_knn(training, testing, k_value):
+def run_knn(training_set, testing_set, k_value):
     return
 
 
@@ -20,10 +23,13 @@ def run_knn(training, testing, k_value):
 #
 if __name__ == "__main__":
     from general_utils import process_args
+    from general_utils import process_data
 
-    (training, testing, mode, k_value) = process_args()
+    (training_file, testing_file, mode, k_value) = process_args()
+    training_set = process_data(training_file)
+    testing_set = process_data(testing_file)
 
     if mode == "NB":
-        run_nb(training, testing)
+        run_nb(training_set, testing_set)
     elif mode == "NN":
-        run_knn(training, testing, k_value)
+        run_knn(training_set, testing_set, k_value)
