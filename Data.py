64686ca82c1d230272e1172fbf8127034795f931
Nathan Ngo <nathanj238@gmail.com>
2020-05-05 00:29:08 +1000

    Created Data object

---
+++ Data.py
@@ -0,0 +1,43 @@
+class Data:
+
+    def __init__(self, attributes, class_value, att_mapping=None):
+        self.attributes = attributes
+        self.class_value = class_value
+        self.att_mapping = att_mapping
+
+    ##
+    # Get a specific attribute
+    #
+    def get_att(self, value, use_mapping=False):
+        if use_mapping and self.att_mapping:
+            index = self.att_mapping[value]
+            return self.attributes[index]
+        elif not use_mapping:
+            return self.attributes[value]
+        else:
+            print("Cannot find attribute, returning None")
+            return None
+
+    ##
+    # Get all attributes
+    # Output: A list containing the outputs.
+    #
+    def get_all_att(self):
+        return self.attributes
+
+    ##
+    # Get the attribute mapping
+    # Output: A set containing the mapping between attribute name and index.
+    #
+    def get_att_map(self):
+        if self.att_mapping:
+            return self.att_mapping
+        else:
+            print("No mapping was found")
+
+    ##
+    # Get the classification of the data entry.
+    # Output: Returns a boolean value representing "yes" or "no".
+    #
+    def get_class_val(self):
+        return self.class_value
