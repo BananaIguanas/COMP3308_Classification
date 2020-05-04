bda6498f03d351d5b987fc734f514d479cfe9a9e
Nathan Ngo <nathanj238@gmail.com>
2020-05-04 20:29:24 +1000

    Fixed style and argument parsing

--- evaluator.py
+++ evaluator.py
@@ -5,19 +5,19 @@ import argparse
 ##
 # Setting up conditions for command line inputs.
 #
-parser = argparse.ArgumentParser(description = "Evaluator for Classifiers")
+parser = argparse.ArgumentParser(description="Evaluator for Classifiers")
 
 parser.add_argument("training",
-                    help = "Location of training file to be split into training and testing sets")
+                    help="Location of training file to be split into training and testing sets")
 
-parser.add_argument("-b", "--naive", action = "store_true",
-                    help = "Run classifier with Naive Bayes algorithm")
+parser.add_argument("mode", choices=["NB", "NN"], default="NB",
+                    help="Run classifier with Naive Bayes algorithm")
 
-parser.add_argument("-n", "--nearest", metavar = "K_VALUE", type = int, dest = 'k',
-                    help = "Run classifier with K Nearest Neighbour Algorithm using given K value")
+parser.add_argument("-k", "--k_value",
+                    help="K value to use with K Nearest Neighbour Algorithm.")
 
-parser.add_argument("-f", "--folds", type = int, default = 10,
-                    help  = "Number of folds the Evaluator should run with")
+parser.add_argument("-f", "--folds", type=int, default=10,
+                    help="Number of folds the Evaluator should run with")
 
 
 ##
@@ -25,6 +25,7 @@ parser.add_argument("-f", "--folds", type = int, default = 10,
 # Output: A tuple. First value is training_set, second is testing_set.
 #
 def seperate_set(fold, total_folds):
+    pass
 
 
 ##
@@ -32,6 +33,7 @@ def seperate_set(fold, total_folds):
 # Output: Percentage value (float) of accuracy.
 #
 def evaluate(classifier_out, true_out):
+    pass
 
 
 ##
@@ -40,6 +42,7 @@ def evaluate(classifier_out, true_out):
 # Note: 'true' represents a classification of 'yes', 'false' represents 'no'.
 #
 def get_true_output(testing_set):
+    pass
 
 
 ##
@@ -47,16 +50,19 @@ def get_true_output(testing_set):
 #
 if __name__ == "__main__":
     cmd_args = parser.parse_args()
-
     total = 0
+
+    if cmd_args == "NN" and not cmd_args.k_value:
+        print("Need to provide K value with Nearest Neighbour.")
+        exit()
 
     for fold in range(cmd_args.folds):
         (training_set, testing_set) = seperate_set(fold, cmd_args.folds)
         true_out = get_true_output(testing_set)
 
-        if cmd_args.naive:
+        if cmd_args.mode == "NB":
             total += evaluate(run_nb(training_set, testing_set), true_out)
         else:
-            total += evaluate(run_nn(training_set, testing_set, cmd_args.k), true_out)
+            total += evaluate(run_knn(training_set, testing_set, cmd_args.k), true_out)
 
     print(f"Average accuracy of {cmd_args.folds} folds: {total/cmd_args.folds}%")
